02bd168efabbf09878cf1dc149f365b64e8ebfb5
pixellateddev <pixellateddev@gmail.com>
2022-03-21 17:34:03 +0530

    a simple mutation

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import { extendType, objectType } from 'nexus';
+import { extendType, intArg, nonNull, objectType, stringArg } from 'nexus';
 
 import { NexusGenObjects } from '../../nexus-typegen';
 
@@ -31,6 +31,42 @@ export const LinkQuery = extendType({
             type: 'Link',
             resolve(parent, args, context, info) {
                 return links
+            }
+        })
+        t.field('link', {
+            type: 'Link',
+            args: {
+                id: nonNull(intArg())
+            },
+            resolve(parent, args, context, info) {
+                const { id } = args
+                const link =  links.find(link => link.id === id)
+                if (link) {
+                    return link
+                }
+                return null
+            }
+        })
+    }
+})
+
+export const LinkMutation = extendType({
+    type: 'Mutation',
+    definition(t) {
+        t.nonNull.field('post', {
+            type: 'Link',
+            args: {
+                description: nonNull(stringArg()),
+                url: nonNull(stringArg())
+            },
+            resolve(parent, args, context, info) {
+                const { description, url } = args
+                const id = links.length + 1
+                const link = {
+                    id, description, url
+                }
+                links.push(link)
+                return link
             }
         })
     }
